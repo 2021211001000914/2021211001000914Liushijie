3b55d32e8fedd59ae6e46ea8fb94e4da60174eee
2021211001000914Liushijie <1499038657@qq.com>
2023-03-21 20:48:11 +0800

    week4

---
+++ src/main/java/com/Liushijie/week4/ConfigDemoServlet.java
@@ -0,0 +1,35 @@
+package com.Liushijie.week4;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@WebServlet(
+        urlPatterns = {"/config"},
+        initParams = {
+                @WebInitParam(name="name",value="Liushijie"),
+                @WebInitParam(name="studentId",value="2021211001000914"),
+        }
+)
+public class ConfigDemoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletConfig config=getServletConfig();
+        String name=config.getInitParameter("name");
+        String studentId=config.getInitParameter("studentId");
+        PrintWriter writer= response.getWriter();
+        writer.println("Name : "+name);
+        writer.println("StudentID : "+studentId);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
